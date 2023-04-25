40d2f97a21f36928f9b86af577eed18d151a0a60
Bug Hunter Yan <39991819+fakeYan@users.noreply.github.com>
2023-04-25 21:20:35 +0800

    Update Module.cpp

--- torch/csrc/Module.cpp
+++ torch/csrc/Module.cpp
@@ -1651,7 +1651,7 @@ Call this whenever a new thread is created in order to propagate values from
   py_module.def(
       "_set_tensor_metadata",
       static_cast<void (*)(
-          const at::Tensor&, std::unordered_map<std::string, bool>)>(
+          const at::Tensor&, std::unordered_map<std::string, int>)>(
           torch::jit::setTensorMetadata));
   py_module.def("_dispatch_key_set", [](const at::Tensor& x) {
     return toString(x.key_set());
